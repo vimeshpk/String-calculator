08120d4cd593ab51435a5c25fb94f8df8328b3ed
vimesh <vimesh.pkv@gmail.com>
2025-08-05 09:56:19 +0530

    Split string value and sum up the array

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,7 +1,9 @@
 class CalculatorsController < ApplicationController
 	def add(numbers)
 	  return 0 if numbers.empty?
+      delimiters = [",", "\n"]
 
-      puts "number present"
+      parts = numbers.split(Regexp.union(delimiters))
+      parts.map(&:to_i).sum
 	end
 end
--- spec/requests/calculators_spec.rb
+++ spec/requests/calculators_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe "Calculators" do
     it "returns 0 for empty string" do
       expect(calculator.add("")).to eq(0)
     end
+
+    it "returns sum for comma separated numbers" do
+      expect(calculator.add("1,2")).to eq(3)
+    end
   end
 end
